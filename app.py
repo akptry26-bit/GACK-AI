48e33f21e3ecfcdaa2419494eef4272bdab75c0b
akptry26-bit <akptry26@gmail.com>
2026-03-04 07:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ model = genai.GenerativeModel('gemini-2.5-flash')
 def get_chat_response(user_input):
     # 1. Google Search tool enable panrom
     model = genai.GenerativeModel(
-        model_name='gemini-1.5-flash',
+        model_name='gemini-2.5-flash',
         tools=[{"google_search_retrieval": {}}] 
     )
 
