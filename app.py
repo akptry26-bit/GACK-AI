b0eea89c939b7e00e7600e090c4397bb06541d86
akptry26-bit <akptry26@gmail.com>
2026-03-04 07:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ else:
 GAC_PROMPT = "You are GAC CORE AI, official assistant for Government Arts College, Karur. If users ask unrelated questions, politely tell them you only handle college queries."
 
 
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-1.5-pro')
 
 
 def get_chat_response(user_input):
     # 1. Google Search tool enable panrom
     model = genai.GenerativeModel(
-        model_name='gemini-2.5-flash',
+        model_name='gemini-1.5-pro', 
         tools=[{"google_search_retrieval": {}}] 
     )
 
